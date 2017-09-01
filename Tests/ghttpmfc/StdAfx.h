454d98a77087260aab9508c7aa6df5b141cab9ea
arves100 <arves100@gmail.com>
2017-09-02 00:09:51 +0200

    Fixed errors for ghttpmfc

--- Tests/ghttpmfc/StdAfx.h
+++ Tests/ghttpmfc/StdAfx.h
@@ -19,7 +19,7 @@
 #include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
-#include "../ghttp.h"
+#include "../../ghttp/ghttp.h"
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
