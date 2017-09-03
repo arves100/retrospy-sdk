a4ae96d7ecb0238a691883485f33a7f1df6544ed
arves100 <arves100@gmail.com>
2017-09-03 17:51:26 +0200

    Target winxp to Voice2MFC

--- Tests/Voice2BuddyMFC/StdAfx.h
+++ Tests/Voice2BuddyMFC/StdAfx.h
@@ -10,6 +10,9 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
+//Target Windows XP
+#define _WIN32_WINNT 0x0501
+
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <afxwin.h>         // MFC core and standard components
